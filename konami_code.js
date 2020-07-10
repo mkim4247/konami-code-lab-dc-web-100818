c8826e01163bb506c5c22c6a16c3cf5246d03c75
Michael Kim <m.kim4247@gmail.com>
2020-07-10 15:18:47 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,22 @@ const codes = [
   "b",
   "a"
 ];
+ 
+let index = 0;
 
 function init() {
-  // your code here
-}
+  document.body.addEventListener("keydown", (event) => {
+    const key = event.key;
+    
+    if (key === codes[index]) {
+      index++;
+      
+      if (index === codes.length) {
+        alert('Hooray!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+)}
